c56d30922ede4297cd669fc17ec9554cbf03de78
hieuit0903 <55152914+hieuit0903@users.noreply.github.com>
2019-09-15 11:24:09 +0700

    Create Lb3B4.java

---
+++ Tuan1/Lab3/Lb3B4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lb3b4;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ *
+ * @author Khuu Hieu
+ */
+public class Lb3B4 extends HocSinh {
+    /**
+     * @param args the command line arguments
+     */
+    public static Scanner sr = new Scanner(System.in);
+    
+   
+public static void main(String[] args) {
+        // TODO code application logic here
+        HocSinh student = new HocSinh();
+        student.nhapDayHocSinh();
+    }
+    
+}
